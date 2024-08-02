e476e169afa2b45dbf6b32e54808fc753b9ab401
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-08-02 20:04:37 +0600

    update

---
+++ assets/js/scripts.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	// Mobile Menu
+	$('.menu_icon').click(function(){
+		$('.menu').slideToggle(200);
+
+		return false
+	});
+
+	// Mobile Menu Icon
+	$(document).ready(function(){
+		$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+			$(this).toggleClass('open');
+		});
+	});
+
+
+	// Banner Carousel
+	// $('.banner_carousel').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	nav:true,
+	// 	dots:true,
+	// 	autoplay:true,
+	// 	responsive : {
+	// 	    0 : {
+	// 	        items:1,
+	// 	    },
+	// 	    480 : {
+	// 	        items:1,
+	// 	    },
+	// 	    768 : {
+	// 	        items:2,
+	// 	    },
+	// 	    992 : {
+	// 	        items:3,
+	// 	    },
+	// 	    1200 : {
+	// 	        items:4,
+	// 	    }
+	// 	}
+	// });
+	// $( ".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+	// $( ".owl-next").html('<i class="fa fa-chevron-right"></i>');
+
+
+
+});
